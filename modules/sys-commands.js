162108ca95196fb80a5b5439448eb06667322e5a
protran <pttran8@uh.edu>
2016-07-23 14:28:07 -0500

    asd
    asd

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -1,3 +1,4 @@
+var http = require('http');
 var fun_mode = true;
 var sysCommands = [funCmd, noFunCmd, idCmd, aboutCmd];
 
@@ -68,7 +69,7 @@ function idCmd(dataHash, callback) {
   var regex = /^\/id$/;
 
   if (regex.test(dataHash.request.text)) {
-    callback(true, "Your groupme id is: HI!");
+    callback(true, "Your groupme id is: HI");
   } else {
     return false;
   }
